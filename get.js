9e549466e8c374697130eee2f4b827b91c5b2f4f
SchorschHarrison <43842888+SchorschHarrison@users.noreply.github.com>
2018-10-04 14:15:13 +0200

    gitignore

--- get.js
+++ get.js
@@ -1,1 +1,10 @@
+var http = require('http');
 
+var client = http.createClient(80, "google.com");
+request = client.request();
+request.on('response', function( res ) {
+    res.on('data', function( data ) {
+        console.log( data );
+    } );
+} );
+request.end();
